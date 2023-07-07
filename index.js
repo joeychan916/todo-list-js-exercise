a59f38dd287559e4b04a0ea2e980bd0e4e93a6a7
Joey Chan <joeychan916@gmail.com>
2023-07-07 23:08:31 +0000

    Refactor Todo List app using JS objects

--- index.js
+++ index.js
@@ -1,31 +1,91 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+// Step 1: Introduce Task Object
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+// Create a new task object with title and description properties
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+    // Method to print the task state
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+    // Method to mark the task as completed
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+
+  return task;
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logTaskState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
+
+// Step 2: Refactor Functions
+
+// Instead of passing taskIndex, pass task object as a parameter
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// Instead of passing taskIndex, pass task object as a parameter
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
+
+// Step 3: Use Methods Instead
+
+// Use task object methods instead of separate functions
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logTaskState() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted() {
+      this.complete = true;
+    }
+  };
+
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logTaskState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
