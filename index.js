82f44e7000793ec3b99c4fa6c9f4841999a57eef
Joey Chan <joeychan916@gmail.com>
2023-07-10 00:26:44 +0000

    Refactor ToDo List app using object-oriented approach and fix method name in Step 3

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // Step 1: Introduce Task Object
 
+// Create a new task object with title and description properties
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
 
-    logState() {
+    // Method to print the task state
+    logTaskState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
-    markCompleted() {
+    // Method to mark the task as completed
+    markCompleted: function() {
       this.complete = true;
     }
   };
+
   return task;
 }
 
@@ -23,9 +27,65 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-task1.logState();
+task1.logTaskState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
-task1.logState();
+task1.logTaskState(); // Clean Cat Litter has been completed
 
 console.log(tasks);
 
+// Step 2: Refactor Functions
+
+// Instead of passing taskIndex, pass task object as a parameter
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// Instead of passing taskIndex, pass task object as a parameter
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+console.log(tasks);
+
+// Step 3: Use Methods Instead
+
+// Use task object methods instead of separate functions
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logTaskState() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted() {
+      this.complete = true;
+    }
+  };
+
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logTaskState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logTaskState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
