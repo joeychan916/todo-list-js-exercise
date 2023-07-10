969c0af6e42440c154f803dd75f7a9ed912163e3
Joey Chan <joeychan916@gmail.com>
2023-07-10 00:28:46 +0000

    Refactor Todo List app using object-oriented approach and implement task methods

--- index.js
+++ index.js
@@ -47,10 +47,6 @@ function completeTask(task) {
 
 // DRIVER CODE BELOW
 
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
 logTaskState(task1); // Clean Cat Litter has not been completed
 completeTask(task1);
 logTaskState(task1); // Clean Cat Litter has been completed
@@ -80,12 +76,9 @@ function newTask(title, description) {
 
 // DRIVER CODE BELOW
 
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
 task1.logTaskState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logTaskState(); // Clean Cat Litter has been completed
 
 console.log(tasks);
+
